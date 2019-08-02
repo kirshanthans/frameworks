948580d215dcfa972c5a2f534fac211545bf38c3
kirshanthans <ksundar@purdue.edu>
2019-08-02 17:11:53 -0400

    installation bug in aby docker fixed

--- aby/install.sh
+++ aby/install.sh
@@ -2,7 +2,6 @@
 # get ABY
 git clone --recursive https://github.com/encryptogroup/ABY.git
 cd ABY
-git checkout -b MPCSOK f52fef85cb8d301440605fb101e1a9e4f7302b04
 
 # copy our working examples 
 for EX in mult3 crosstabs innerprod
